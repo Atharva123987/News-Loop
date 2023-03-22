6cd9acc9d27131712ee401b424fe8d4f04ae5dcc
Atharva123987 <atharva.amberkar@gmail.com>
2023-03-22 16:32:23 +0530

    Searching
    Searchbar updates and search results page

---
+++ pages/searchresult.js
@@ -0,0 +1,25 @@
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import { useState } from "react";
+export const searchresult = ({data}) =>{
+    const [results, setResults] = useState(data);
+    return(
+        <>
+        <NavBar/>
+        <div className="grid grid-cols-3 2xl:flex 2xl:flex-row">
+        
+          {/* {
+            results.map((e, i) => {
+              return (<>
+                <NewsBox key={i} title={e.title} description={e.description} url={e.link} urlToImage={e.image_url} />
+              </>
+              )
+            })
+          } */}
+        </div>
+        <Footer/>
+
+        </>
+    )
+}
+export default searchresult;
