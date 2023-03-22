ca59ec8c0d91210b63033398c6b3e1270862a90a
Atharva123987 <atharva.amberkar@gmail.com>
2023-03-22 18:18:03 +0530

    Update searchresult.js

--- pages/searchresult.js
+++ pages/searchresult.js
@@ -1,29 +1,29 @@
-import Footer from "@/components/Footer";
-import NavBar from "@/components/NavBar";
-import { useState } from "react";
-export const Searchresult = ({articles}) =>{
-    const [results, setResults] = useState(data);
+// import Footer from "@/components/Footer";
+// import NavBar from "@/components/NavBar";
+// import { useState } from "react";
+// export const Searchresult = ({articles}) =>{
+//     const [results, setResults] = useState(data);
 
-    function handleData(){
+//     function handleData(){
       
-    }
-    return(
-        <>
-        <NavBar/>
-        <div className="grid grid-cols-3 2xl:flex 2xl:flex-row">
+//     }
+//     return(
+//         <>
+//         <NavBar/>
+//         <div className="grid grid-cols-3 2xl:flex 2xl:flex-row">
         
-          {/* {
-            results.map((e, i) => {
-              return (<>
-                <NewsBox key={i} title={e.title} description={e.description} url={e.link} urlToImage={e.image_url} data={handleData} />
-              </>
-              )
-            })
-          } */}
-        </div>
-        <Footer/>
+//           {/* {
+//             results.map((e, i) => {
+//               return (<>
+//                 <NewsBox key={i} title={e.title} description={e.description} url={e.link} urlToImage={e.image_url} data={handleData} />
+//               </>
+//               )
+//             })
+//           } */}
+//         </div>
+//         <Footer/>
 
-        </>
-    )
-}
-export default Searchresult;
+//         </>
+//     )
+// }
+// export default Searchresult;
